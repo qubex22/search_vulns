c521fa66a490a1228999b5582aa1fe89149a8de9
cube <joroga2212@gmail.com>
2025-02-19 13:54:18 +0100

    Update install.sh
    separar paquetes de db

--- install.sh
+++ install.sh
@@ -3,47 +3,6 @@
 QUIET=0
 FULL_RESOURCE_INSTALL=0
 SKIP_RESOURCE_INSTALL=0
-LINUX_PACKAGE_MANAGER="apt-get"
-
-install_linux_packages() {
-    # Install required packages
-    PACKAGES="python3 python3-pip wget curl sqlite3 libsqlite3-dev cmake gcc libmariadb-dev mariadb-client jq"
-    which ${LINUX_PACKAGE_MANAGER} &> /dev/null
-    if [ $? != 0 ]; then
-        printf "${RED}Could not find ${LINUX_PACKAGE_MANAGER} command.\\nPlease specify your package manager at the start of the script.\\n${SANE}"
-        exit 1
-    fi
-
-    if [ $QUIET != 1 ]; then
-        sudo ${LINUX_PACKAGE_MANAGER} update
-    else
-        sudo ${LINUX_PACKAGE_MANAGER} update >/dev/null
-    fi
-    if [ $? != 0 ]; then
-        printf "${RED}Installation of ${LINUX_PACKAGE_MANAGER} packages was not successful.\\n${SANE}"
-        exit 1
-    fi
-
-    if [ ${QUIET} != 1 ]; then
-        sudo ${LINUX_PACKAGE_MANAGER} -y install ${PACKAGES}
-    else
-        sudo ${LINUX_PACKAGE_MANAGER} -y install ${PACKAGES} >/dev/null
-    fi
-    if [ $? != 0 ]; then
-        printf "${RED}Installation of ${LINUX_PACKAGE_MANAGER} packages was not successful.\\n${SANE}"
-        exit 1
-    fi
-
-    pip3 install -r requirements.txt
-    if [ $? != 0 ]; then
-        pip3 install -r requirements.txt --break-system-packages
-    fi
-
-    pip3 install mariadb
-    if [ $? != 0 ]; then
-        pip3 install mariadb --break-system-packages
-    fi
-}
 
 setup_create_db() {
     ## configure submodules of SQLiteCpp for create_db
@@ -153,8 +112,6 @@ if [ $# -gt 0 ]; then
 fi
 
 # run script
-printf "${GREEN}[+] Installing ${LINUX_PACKAGE_MANAGER} packages\\n${SANE}"
-install_linux_packages
 printf "${GREEN}[+] Setting up cpe_search tool\\n${SANE}"
 setup_cpe_search
 printf "${GREEN}[+] Setting up vulnerability database creation tool\\n${SANE}"
@@ -166,5 +123,3 @@ else
     printf "${GREEN}[-] Skipping install of vulnerability and software database\\n${SANE}"
 fi
 
-
-sudo ln -sf "$(pwd -P)/search_vulns.py" /usr/local/bin/search_vulns
